b333154fe37168a234e35c293c3a2621b891d2e5
Nito Buendia <git@nitobuendia.com>
2025-04-28 09:53:25 +0800

    refactor(scripts): add documentation to package script

--- scripts/package.js
+++ scripts/package.js
@@ -1,23 +1,33 @@
+/** @file Creates a zip file from dist to use as a Chrome Extension. */
+
 const AdmZip = require('adm-zip');
 const path = require('path');
 const fs = require('fs');
 
 const distPath = path.resolve(__dirname, '../dist');
-const buildZipPath = path.resolve(__dirname, '../build.zip');
+const buildZipPath = path.resolve(__dirname, '../lazy-shortcuts-extension.zip');
 
-// Ensure the dist folder exists
-if (!fs.existsSync(distPath)) {
-  console.error('Error: The "dist" folder does not exist. Run "npm run build" first.');
-  process.exit(1);
-}
+/**
+ * Creates a Zip file from the /dist/ folder.
+ * Must run `npm run build` first to create the dist folder.
+ */
+const createZipExtension = () => {
+  if (!fs.existsSync(distPath)) {
+    console.error(
+      'Error: The "dist" folder does not exist. Run "npm run build" first.'
+    );
+    process.exit(1);
+  }
 
-const zip = new AdmZip();
-zip.addLocalFolder(distPath);
+  try {
+    const zip = new AdmZip();
+    zip.addLocalFolder(distPath);
+    zip.writeZip(buildZipPath);
+    console.log(`Successfully created ${buildZipPath}`);
+  } catch (e) {
+    console.error('Error creating zip file:', e);
+    process.exit(1);
+  }
+};
 
-try {
-  zip.writeZip(buildZipPath);
-  console.log(`Successfully created ${buildZipPath}`);
-} catch (e) {
-  console.error('Error creating zip file:', e);
-  process.exit(1);
-}
+createZipExtension();
